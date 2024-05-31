99060aaa9a92e2ef72a3ac5a29314728e8f2accc
Aaron <awayfromserver@gmail.com>
2024-05-31 16:33:37 -0400

    100% coverage on main

--- gobot.go
+++ gobot.go
@@ -9,19 +9,19 @@ import (
 	"github.com/awayfromserver/gobot/bot"
 )
 
+var (
+	BotChannel = make(chan os.Signal, 1)
+)
+
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
+	fmt.Println("Bot setup...")
 	bot.Setup()
+	fmt.Println("Bot starting up...")
 	bot.Run(ctx)
 
-	testmode, ok := os.LookupEnv("TESTMODE")
-	if !ok || testmode == "true" {
-		fmt.Println("We're in test mode. Shutting down...")
-	} else {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-		<-c
-		fmt.Println("Bot shutting down...")
-	}
+	signal.Notify(BotChannel, os.Interrupt)
+	<-BotChannel
+	fmt.Println("Bot shutting down...")
 	cancel()
 }
--- gobot_test.go
+++ gobot_test.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"os"
+	"syscall"
 	"testing"
 )
 
-func TestMn(t *testing.T) {
+func TestFuncMain(t *testing.T) {
 	bt, ok := os.LookupEnv("BOT_TOKEN")
 	if !ok || bt == "" {
 		t.Skip("BOT_TOKEN isn't set")
 	}
-	t.Setenv("TESTMODE", "true")
 
-	main()
+	go main()
+	BotChannel <- syscall.SIGINT
 }
