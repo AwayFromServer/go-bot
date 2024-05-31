69467ba3f0cb88c6e5be99c7fda6a35547f12a1b
Aaron <awayfromserver@gmail.com>
2024-05-31 04:18:26 -0400

    Adding gobot_test.go

---
+++ gobot_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMn(t *testing.T) {
+	bt, ok := os.LookupEnv("BOT_TOKEN")
+	if !ok || bt == "" {
+		t.Skip("BOT_TOKEN isn't set")
+	}
+
+	main()
+}
